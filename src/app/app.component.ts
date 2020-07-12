fe4c4b4a8a6a9211e06b9530942e63ada64b4f21
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-07-12 10:16:54 +0800

    step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'newProj';
+  title = '這是標題';
+  subtitle = 'A place to share your knowledge.';
 }
