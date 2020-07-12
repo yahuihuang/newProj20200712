1c9889ae1f54d25c2708d448fcd18bb8b4232ba1
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-07-12 09:43:51 +0800

    step1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ArgiclesComponent } from './argicles/argicles.component';
+import { TagsComponent } from './tags/tags.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArgiclesComponent,
+    TagsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
